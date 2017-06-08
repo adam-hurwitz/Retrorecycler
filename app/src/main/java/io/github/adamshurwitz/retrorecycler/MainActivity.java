fda594e2d8e27a4ab1bf179e0047a841bfc58c6a
Adam Hurwitz <ahurwitz@ebay.com>
2017-06-08 08:37:16 -0700

    refactor: adding CompositeSubscription and onError

--- app/src/main/java/io/github/adamshurwitz/retrorecycler/MainActivity.java
+++ app/src/main/java/io/github/adamshurwitz/retrorecycler/MainActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.Toast;
 
 
 import io.github.adamshurwitz.retrorecycler.RecyclerView.Adapter;
+import rx.subscriptions.CompositeSubscription;
 
 import java.util.ArrayList;
 
@@ -28,9 +30,13 @@ public class MainActivity
 
     private static final String NO_HOMEPAGE = "No Homepage Provided";
 
+    private CompositeSubscription compositeSubscription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        compositeSubscription = new CompositeSubscription();
 
         setContentView(R.layout.activity_main);
 
@@ -44,6 +50,22 @@ public class MainActivity
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (compositeSubscription != null) {
+            compositeSubscription.clear();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        compositeSubscription = new CompositeSubscription();
+        adapter.addItems(courses);
+        adapter.swapItems(courses);
+    }
+
     private void initRecyclerView() {
 
         adapter = new Adapter(this);
@@ -52,37 +74,35 @@ public class MainActivity
         recyclerView.setHasFixedSize(true);
         onIndexClickedEvent();
         onCourseClickedEvent();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        adapter.addItems(courses);
-        adapter.swapItems(courses);
 
     }
 
     private void onIndexClickedEvent() {
-        adapter.indexClickedEvent().subscribe(pair -> {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(Urls.courseCatalog));
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                startActivity(intent);
-            }
-        });
+        compositeSubscription.add(
+                adapter.indexClickedEvent().subscribe(pair -> {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(Urls.courseCatalog));
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
+                }, throwable -> {
+                    Log.e(MainActivity.class.getSimpleName(), throwable.toString());
+                }));
     }
 
     private void onCourseClickedEvent() {
-        adapter.courseClickedEvent().subscribe(pair -> {
-            if (pair.first != null && !pair.first.isEmpty()) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(pair.first));
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                }
-            } else {
-                Toast.makeText(this, NO_HOMEPAGE, Toast.LENGTH_SHORT).show();
-            }
-        });
+        compositeSubscription.add(
+                adapter.courseClickedEvent().subscribe(pair -> {
+                    if (pair.first != null && !pair.first.isEmpty()) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(pair.first));
+                        if (intent.resolveActivity(getPackageManager()) != null) {
+                            startActivity(intent);
+                        }
+                    } else {
+                        Toast.makeText(this, NO_HOMEPAGE, Toast.LENGTH_SHORT).show();
+                    }
+                }, throwable -> {
+                    Log.e(MainActivity.class.getSimpleName(), throwable.toString());
+                }));
     }
 
     public void getData(ArrayList<Model.Course> data) {
