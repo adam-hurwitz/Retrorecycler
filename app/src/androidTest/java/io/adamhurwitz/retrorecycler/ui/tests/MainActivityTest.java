19332dbc143ab71ab85f75bc95bc50e42be85ca0
Adam Hurwitz <ahurwitz@ebay.com>
2017-06-11 22:06:31 -0700

    test: espresso testing

---
+++ app/src/androidTest/java/io/adamhurwitz/retrorecycler/ui/tests/MainActivityTest.java
@@ -0,0 +1,54 @@
+package io.adamhurwitz.retrorecycler.ui.tests;
+
+import android.os.SystemClock;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import io.github.adamshurwitz.retrorecycler.MainActivity;
+import io.github.adamshurwitz.retrorecycler.R;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by ahurwitz on 6/11/17.
+ */
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testScreenLoad() {
+        onView(withText("RetrofitRecycler")).check(matches(isDisplayed()));
+    }
+
+    //todo: debug
+    /*@Test
+    public void scrollToCourseCatalog() {
+        // First scroll to the position that needs to be matched and click on it.
+        onView(ViewMatchers.withId(R.id.recycler_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        // Match the text in an item below the fold and check that it's displayed.
+        onView(withText("COURSE CATALOG")).check(matches(isDisplayed()));
+    }*/
+
+}
+
